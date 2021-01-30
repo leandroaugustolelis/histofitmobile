b13d016650af6bca7716171a66b9a143cbb912f9
Leandro Lelis <leandroaugustolelis@gmail.com>
2021-01-30 00:46:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from '../routes';
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
